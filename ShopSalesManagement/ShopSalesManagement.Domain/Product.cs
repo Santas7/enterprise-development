dc697d0465376ef02dc9c79beb93f879ca4c4195
Santas7 <andreypanyshkin03@gmail.com>
2024-11-03 13:23:16 +0300

    исправляю warnings

--- ShopSalesManagement/ShopSalesManagement.Domain/Product.cs
+++ ShopSalesManagement/ShopSalesManagement.Domain/Product.cs
@@ -7,11 +7,11 @@ namespace ShopSalesManagement.Domain
     {
         [Key]
         public int Id { get; set; }
-        public string Barcode { get; set; }
+        public required string Barcode { get; set; }
         public int ProductGroupId { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public decimal Weight { get; set; }
-        public string Type { get; set; }
+        public required string Type { get; set; }
         public decimal Price { get; set; }
         public DateTime ExpirationDate { get; set; }
         public ProductGroup ProductGroup { get; set; }
